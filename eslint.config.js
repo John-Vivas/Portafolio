be876a014d13f688d74fe7d2b19bf148cede382d
John-Vivas <87059223+John-Vivas@users.noreply.github.com>
2024-11-21 14:28:19 -0500

    change  in esliny.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
-    files: ['./src/**/*.{js,ts,jsx,tsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
